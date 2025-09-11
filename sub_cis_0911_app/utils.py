a1bfc0b8e6d5c264f40f0f567df58c4d24647f1b
shin <shindou0225@gmail.com>
2025-09-11 17:58:47 +0900

    update utils.py

--- sub_cis_0911_app/utils.py
+++ sub_cis_0911_app/utils.py
@@ -13,7 +13,7 @@ from langchain.schema import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-import CISearchApp.constants as ct
+from . import constants   #同一ディレクトリ内のconstants.pyをインポート
 
 
 ############################################################
